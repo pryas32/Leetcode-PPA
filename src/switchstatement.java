44a4a9faf58003edbfd980bd1da44033a81191e9
pryas32 <shuklaaayush805@gmail.com>
2025-01-18 21:49:44 +0530

    final commit

---
+++ src/switchstatement.java
@@ -0,0 +1,27 @@
+public class switchstatement {
+
+    //when to use switch statement->when there is a fixed variable and it has multiple branches
+public static void main(String args[]){
+
+
+   // int a=23;
+char a='L';
+
+    switch (a){ // or we can use char or string here.
+        case 'A':
+            System.out.print("hello ");
+            break;
+        case 'L':
+            System.out.print("hello world ");
+            break;
+        default:
+            System.out.print("nothing matches");
+    }
+
+
+
+}
+
+
+
+}
