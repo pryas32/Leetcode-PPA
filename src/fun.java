b29a57d5799c427e57cb9fef0380fb98bff6b22b
pryas32 <shuklaaayush805@gmail.com>
2025-01-19 02:34:33 +0530

    final commit

--- src/fun.java
+++ src/fun.java
@@ -5,8 +5,9 @@ int y;
 
 
 }
-
-
+applications of fumctions -> act as a stack when one fucntion is called until umless that is not being complted the other will not be called.
+//it follows fucnction call stack.
+advantage of functions->reduces  code redundancy.
  class abc{
     public static void main(String args []){
 
