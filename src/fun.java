34f9c0fc6189b44bd9541683752eca9cc174a033
pryas32 <shuklaaayush805@gmail.com>
2025-01-19 02:30:27 +0530

    final commit

---
+++ src/fun.java
@@ -0,0 +1,25 @@
+public class fun {
+
+int x;
+int y;
+
+
+}
+
+
+ class abc{
+    public static void main(String args []){
+
+        fun f=new fun();
+        f.x=10;
+        func(f);
+
+        System.out.print(f.x+" "+f.y);
+    }
+
+public static void func(fun f){
+      f=new fun();
+
+}
+
+}
