8ff43f5671a5bacd09d6692ea0a0a15279d79f3d
pryas32 <shuklaaayush805@gmail.com>
2025-01-19 02:25:03 +0530

    final commit

---
+++ src/functions.java
@@ -0,0 +1,34 @@
+public class functions {
+    public static void main(String args[]){
+
+
+int a =23,b=11;
+
+//primitive data type arent passed as a refrence
+        //while non primitive data type are passed as refrence.
+        //means changes will be reflected .
+
+        //but but but taking an example
+fun(a,b);
+
+System.out.print(a);
+        System.out.print(b);
+
+
+    }
+
+    public static void fun(int a ,int b){
+
+
+
+        a+=1;
+        b+=1;
+
+
+    }
+
+
+
+
+
+}
