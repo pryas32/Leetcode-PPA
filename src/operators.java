4bd5d35b485a68f08807cf19668864ea54ec907c
pryas32 <shuklaaayush805@gmail.com>
2025-01-18 16:02:53 +0530

    final commit

---
+++ src/operators.java
@@ -0,0 +1,20 @@
+public class operators {
+assignment operator(= ,+=,-+,*=,%=)
+    in assgnment operator the associativity goes from R to L
+            arthimetic -> in arithemetic the associatitvity goes from L to R
+    int x=(a*b+c); the associavity goes from L to R;
+    relational operator
+           logical(&&,||,!=)
+ short-cicuiting(a && b)->hota hain so only a and we wont check b
+            similarly a||b -
+    > will check a and not b
+            relational operator used for comparing two things(>=,<=,==,>,<);
+
+
+
+
+
+
+
+
+}
