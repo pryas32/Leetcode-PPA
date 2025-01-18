54d5af60cc0952cfed37e9d840ea9439fccafd4c
pryas32 <shuklaaayush805@gmail.com>
2025-01-18 19:03:59 +0530

    final commit

---
+++ src/rightleftshiftoperation.java
@@ -0,0 +1,34 @@
+public class rightleftshiftoperation {
+
+    public static void main(String args[]){
+
+//        left left ->in leftr shit we append 0 << whether its negative or positive
+
+
+        System.out.print(3<<1);
+        System.out.print(-3<<1);
+
+//        for right shift
+//                for signed bit for negative no its append 1 instread of zero taking care of sign
+
+//        signed bit sysbol ->>
+//                unsigned bit ->>>>
+
+System.out.println(-2>>>1);
+System.out.println(-2>>1);
+
+//        not operator ~
+
+
+//        2^32-1-x
+
+
+      System.out.print(~2);
+//      2^32-1-2 ie 3
+
+
+
+
+
+    }
+}
