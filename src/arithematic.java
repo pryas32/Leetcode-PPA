4ef86e8e707d60ec234e50f0e19e6d9c880aef1a
pryas32 <shuklaaayush805@gmail.com>
2025-01-18 18:52:49 +0530

    final commit

---
+++ src/arithematic.java
@@ -0,0 +1,25 @@
+public class arithematic {
+
+public static void main(String args[]) {
+//    aithematic and
+//            arithemtic or
+//                    arithemetic XOR
+//                            arithemetic not
+//
+//
+//                                    1&0=0;1|0=1
+    System.out.println(3 & 4);
+    System.out.println(3 | 4);
+
+//    same bit then zero
+//            opposite bit then one
+//    XOR
+//    1 & 1=0;
+//    0 & 1=1;
+    System.out.println(3^4);
+
+}
+
+
+
+}
