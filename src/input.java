41f5639c1e82bd9abe9c53a60682dc49baca3109
pryas32 <shuklaaayush805@gmail.com>
2025-01-18 13:46:43 +0530

    final commit

--- src/input.java
+++ src/input.java
@@ -6,23 +6,25 @@ import static java.lang.Integer.parseInt;
 
 public class input {
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) {
 
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));//System.in->used to read byte
+        //  BufferedReader br=new BufferedReader(new InputStreamReader(System.in));//System.in->used to read byte
 //InputStreamReader->it convert stream of bytes inot stream of character
-       //BufferedReader->to read character from stream of character .
+        //BufferedReader->to read character from stream of character .
 
-         String s=br.readLine();//three types there read for reading a character //read a line of character
+        // String s=br.readLine();//three types there read for reading a character //read a line of character
         //readline()->for reading a line
-
-int x=parseInt(s);
-System.out.println(x);
-    }
+//
+//int x=parseInt(s);
+//System.out.println(x);
+//    }
 //    there are mainly two ways of taking input in java
 //            1.BufferReader
 //            2.Scanner class
 
 
+//althugh bufferreader is faster than Scanner still we prefer Scanner as we dont need of checked exception so far.
 
 
+    }
 }
