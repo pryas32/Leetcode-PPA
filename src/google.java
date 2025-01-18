a3cec8a009a7994639665295f3ebd963dd2aa853
pryas32 <shuklaaayush805@gmail.com>
2025-01-18 14:18:52 +0530

    final commit

--- src/google.java
+++ src/google.java
@@ -4,22 +4,7 @@ import java.util.Scanner;
 public class google {
 
     public static void main(String args[]){
-    Scanner sc=new Scanner(System.in);
 
-    float d=sc.nextFloat();
-    System.out.println(d);
-  //  int x=sc.nextInt();
-
-//    String s=sc.nextLine();
-   // char ch=sc.nextLine().charAt(0);
-
-   // double d=sc.nextDouble();
-
-   // System.out.println(x);
-//    System.out.println(s);
-     //System.out.println(ch);
-
-        //bufferreader is prefered over scanner in mutip threaded evn and it thorws IOException
 
 }
 }
---
+++ src/output.java
@@ -0,0 +1,19 @@
+public class output {
+
+    public static void main(String args[]){
+//output has format
+//System.out.println();
+//or System.out.format() for dates and all;
+
+
+//escape sequences ->for escaping everything ->\t,\\,\",\n
+float y=3.142323f;
+double d=23.343434d;
+System.out.format("%2.1f",y);
+
+
+
+
+
+    }
+}
