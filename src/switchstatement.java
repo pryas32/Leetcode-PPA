ff633af08a8de3915db259803b269066d1318869
pryas32 <shuklaaayush805@gmail.com>
2025-01-19 14:37:05 +0530

    final commit

--- src/switchstatement.java
+++ src/switchstatement.java
@@ -1,27 +1,33 @@
 public class switchstatement {
 
     //when to use switch statement->when there is a fixed variable and it has multiple branches
-public static void main(String args[]){
+    public static void main(String args[]) {
 
 
-   // int a=23;
-char a='L';
+        // int a=23;
+//char a='L';
+//
+//    switch (a){ // or we can use char or string here.
+//        case 'A':
+//            System.out.print("hello ");
+//            break;
+//        case 'L':
+//            System.out.print("hello world ");
+//            break;
+//        default:
+//            System.out.print("nothing matches");
 
-    switch (a){ // or we can use char or string here.
-        case 'A':
-            System.out.print("hello ");
-            break;
-        case 'L':
-            System.out.print("hello world ");
-            break;
-        default:
-            System.out.print("nothing matches");
+        int a = 5;
+
+        switch (a++) {
+            case 5:
+                System.out.println("First");
+                System.out.print(a);
+            case 6:
+                System.out.println(a++);
+                break;
+            case 7:
+                System.out.println("YES");
+        }
     }
-
-
-
-}
-
-
-
 }
