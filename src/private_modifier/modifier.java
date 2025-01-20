e4e0d468203a5b0dfa67bb83afcae17916544db5
pryas32 <shuklaaayush805@gmail.com>
2025-01-21 01:44:04 +0530

    final commit

---
+++ src/private_modifier/modifier.java
@@ -0,0 +1,20 @@
+package private_modifier;
+
+public class modifier {
+
+
+
+        private int a;
+        private int b;
+
+
+        public void print() {
+            System.out.println("private modifier cant be accessed");
+        }
+
+
+        //private modifier cant be accessed outside the class
+
+
+    }
+
