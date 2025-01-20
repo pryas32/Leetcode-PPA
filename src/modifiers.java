9c01e1da5ad0170e1a1dad774015090eb7eb4dc5
pryas32 <shuklaaayush805@gmail.com>
2025-01-21 00:44:43 +0530

    final commit

---
+++ src/modifiers.java
@@ -0,0 +1,18 @@
+public class modifiers {
+
+    //modifiers decide to whcih range your classes properties are accessible
+
+    //four type of modifiers
+
+   //  private  modifiers ->they have propertesaccesible within the class itself outside will give error
+    //public -> con be accessed anywhere ->withing class and outwixe the class
+    //package ->collections of classes-> a packgae cant have more than one classes with same name.
+    //default-> if no modifier mentioned then its default one can be accessed withing the pkg
+    //protected->can be accessed within the package and the child class of some other pkg
+
+
+
+
+
+
+}
