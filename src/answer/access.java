165682b0acf51a2421c2926d783c6c835f7a057a
pryas32 <shuklaaayush805@gmail.com>
2025-01-21 01:43:42 +0530

    final commit

---
+++ src/answer/access.java
@@ -0,0 +1,12 @@
+package answer;
+
+import private_modifier.modifier;
+
+public class access {
+
+    modifier f=new modifier();
+    System.out.println(f.a); //private modifier cant be accessed outside the class.
+
+
+
+}
