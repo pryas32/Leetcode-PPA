74ae558231d1aa4620ad5407e50608b647378262
pryas32 <shuklaaayush805@gmail.com>
2025-01-21 11:17:20 +0530

    final commit

---
+++ src/private_modifier/static_properties.java
@@ -0,0 +1,15 @@
+package private_modifier;
+
+public class static_properties {
+    //they are the properties at thh class level
+    //they can be accessed through the class name
+    //they can be modified in a non-static method
+    //they dont have refrence to this keyword as they can be accssed through class name
+    //althrough we can access static member through object as well
+
+
+
+
+
+
+}
