034b4336c2b4a594a3358ac52ae07a4c06a7fa59
pryas32 <shuklaaayush805@gmail.com>
2025-01-23 01:59:54 +0530

    final commit

---
+++ src/differerence_abstract_interface.java
@@ -0,0 +1,24 @@
+public class differerence_abstract_interface {
+
+//similarity
+    //cant create intances of both
+    //can point to other refrences
+
+
+    //difference
+    //interface use when we dotn want constructor
+
+
+    //when to use abtraction
+    //when is-a-relationship like area
+    //when want to use public ,private and protected methiods
+    //when  we need non static  as well as non final mmebers  .
+    //a class can implements multiple interface
+    //an interface can extends multiple interface.
+
+
+
+
+
+
+}
