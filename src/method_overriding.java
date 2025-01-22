ce4893a1fc860cf0a47c6785510a0a67cb3f1ddf
pryas32 <shuklaaayush805@gmail.com>
2025-01-22 11:15:48 +0530

    final commit

--- src/method_overriding.java
+++ src/method_overriding.java
@@ -7,5 +7,5 @@ class method_overriding {
         System.out.println("base class");
     }
 //
-
+ //
 }
--- src/method_overriding_child.java
+++ src/method_overriding_child.java
@@ -9,7 +9,7 @@ public static void main(String args[]){
     method_overriding m=new method_overriding_child();
     m.print();  //in method overriding base class refering to the class one is called
     //at runtime if method is not present to the class to which its pointing out then parent class method is called.
-
+//
 //Note static method cant be overridden in java
     //eg as given above .
 
