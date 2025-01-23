e7257d673c9a4abfb7ae7c543109962ca0a7ea67
pryas32 <shuklaaayush805@gmail.com>
2025-01-24 00:57:10 +0530

    final commit

---
+++ src/BigNumber.java
@@ -0,0 +1,53 @@
+import java.math.BigInteger;
+
+public class BigNumber {
+
+    public static void main(String args[]) {
+//        BigInteger b = new BigInteger("122343");
+////string to biginteger
+//
+//
+//        System.out.println(b);
+//
+////String to bigInteger
+//        String s = "pryas_gupta";
+//        BigInteger b1 = new BigInteger(s);
+//
+//
+//        int a=23;
+//
+//        BigInteger b11=BigInteger.valueOf(a);
+
+        //integer to bigInteger above example
+
+BigInteger a1=new BigInteger("231");
+BigInteger ba=new BigInteger("21");
+
+BigInteger c1=a1.add(ba);
+
+//biginteger to String
+        //biginteger to long
+        //biginteger to interger
+
+    BigInteger aa=new BigInteger("11");
+int aaa=aa.intValue();
+
+   long aaaa=aa.longValue();
+
+
+String aaaaa=aa.toString();
+
+System.out.println(aaa);
+        System.out.println(aaaa);
+        System.out.println(aaaaa);
+
+
+
+
+System.out.println(c1);
+
+
+
+
+    }
+}
