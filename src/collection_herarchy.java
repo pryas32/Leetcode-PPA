98fa0609af694ed9cb24bd5c37be19d3dc4f08c7
pryas32 <shuklaaayush805@gmail.com>
2025-01-24 00:03:09 +0530

    final commit

---
+++ src/collection_herarchy.java
@@ -0,0 +1,25 @@
+public class collection_herarchy {
+    //its a collection of herarchy used for implementation of basc data structure
+
+    //list->arraylist,linkedlist,stack ,vector
+        //    (its an implemnetation of dynamic size array)   (linkedlist is implementation of doubly linkedlist) stack and vector are thread safe class
+    //instead of stack we can use arraylist and instead of vector we can use arraydequeue
+    //map->hashmap->implementation of hashing,
+    // treemap(self balancing binary search tree) sorted order
+    //linkedhashmap->implemntation of hashmap and linkedlist (maintain insertion order)
+    //set->hashset(implementaed using hashing)
+    //treeset ->implemnedt using self balancing red black tree.
+    //linkedhashset->implemented using (maint insertion order
+    //queue->linkedlist ,priority_queue(implemnretd using heap),arradequeue(insertion and deletion from both the ends)
+    //dequeue->arraydequeue,linkedlist
+//(dequeue->insertion and delettion from both the ends )
+
+//collections->it inherit from the object class
+    //it has functions like min ,max ,binary_search etch which can be applied on above data structure
+
+
+
+
+
+
+}
