44cedf86d2cecae53c0772e04bf766a3c106c1e5
pryas32 <shuklaaayush805@gmail.com>
2025-01-25 01:57:47 +0530

    final commit

---
+++ src/lambdaexpressionsyntax.java
@@ -0,0 +1,15 @@
+public class lambdaexpressionsyntax {
+
+
+    //lambda expression used for single line or for small level code not for multple line code
+    //like  if()return true else if return this else if return this //here lambda expression are avoided.
+
+    p->System.out.println(p)//single argument
+
+    p->{return p+2} can be changed to p->{p+2};
+
+
+
+
+
+}
