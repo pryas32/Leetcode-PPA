79411725447cf0e0c0a27f8322f6abe9aefef486
pryas32 <shuklaaayush805@gmail.com>
2025-01-26 00:52:48 +0530

    final commit

---
+++ src/Exception_herarchy.java
@@ -0,0 +1,27 @@
+public class Exception_herarchy {
+
+
+
+          Object->its a  top class of java .every class inherit from this class)
+            |
+   Throwable->(it is the super class  .All class inherti from this class
+      |   |        in case of exception.It has methods like printstacktrace().every exception realted
+      |   |       class implement this method)
+      |   |
+      |   |
+      |   |
+    Error-| These are not in  our hand.like IO error /some file deleted
+           |
+          |
+        Exception -> thwse are the error which occur in program and its in our hand we need to handle it.
+
+
+   Exception are categorized into two categories
+            1.checked excetion-> they are cheked at compile time .we needd to write extra code to handle it
+    eg file not found.else jave will not compile the code
+
+
+            1.Runtime exception -> they are chejked at runtime eg arthemetic exception,array index out of bound exception ,null poiner exception
+            compiler will not force me to  writre extra code here to handle them.
+
+}
