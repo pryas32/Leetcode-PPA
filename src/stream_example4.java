1b27a3c269efd1f93fc6c86d2af56edef77cc9bf
pryas32 <shuklaaayush805@gmail.com>
2025-01-25 17:44:11 +0530

    final commit

--- src/stream_example4.java
+++ src/stream_example4.java
@@ -26,27 +26,27 @@ int getmarks(){
         return name;
     }
 
-public static void main(String args[]){
+public static void main(String args[]) {
 
 
-        stream_example4 student[]= {new stream_example4(10, "prayas", 13),
-                new stream_example4(10, "gupta", 63),
-                new stream_example4(10, "okgoogle", 34)};
+    stream_example4 student[] = {new stream_example4(10, "prayas", 13),
+            new stream_example4(10, "gupta", 63),
+            new stream_example4(10, "okgoogle", 34)};
 
 
-    double summation =Arrays.stream(student).mapToInt(x->x.getmarks())//convert from non primitive to primitive
+    double summation = Arrays.stream(student).mapToInt(x -> x.getmarks())//convert from non primitive to primitive
             .average().getAsDouble();
 
 
-System.out.println(summation);
+    System.out.println(summation);
 
 //giving example of collect method how to collect or groupby streams
 
 //like we want to collect in map
 
-    Map<Integer ,String>arr;
+    Map<Integer, String> arr;
     arr = Arrays.stream(student).collect(Collectors.toMap(//giving refrence of key an value here
-            stream_example4::getmarks,stream_example4::getnames));
+            stream_example4::getmarks, stream_example4::getnames));
 
     arr.forEach((marks, name) -> System.out.println("Marks: " + marks + ", Name: " + name));
 
@@ -54,11 +54,14 @@ System.out.println(summation);
     //now using groupby
 
 
-    Map<Integer , List<stream_example4>>arraytest;
+    Map<Integer, List<stream_example4>> arraytest;
     arraytest = Arrays.stream(student).collect(Collectors.groupingBy(stream_example4::getmarks));
 
     arraytest.forEach((marks, names) -> System.out.println("Marks: " + marks + ", Names: " + names));
 
-
+//for strings ,arrays as they dont implemnt stream so for makeing them as source we need to do
+    //as Arrays.Stream(String_name);
+    //Arrays.Stream(arrays);
+}
     }
         }
