05ec1e89807d9b0b728ea742d6ddf8c336d9ed03
pryas32 <shuklaaayush805@gmail.com>
2025-01-26 15:44:19 +0530

    final commit

--- src/Exception_herarch.java
+++ src/Exception_herarch.java
@@ -4,48 +4,61 @@ import java.util.Scanner;
 public class Exception_herarch {
 
 
-    there are mainly five keyword
+//    there are mainly five keyword
+//
+//
+//
+//    try {
+//        //code that can throw an exception put it inot this block
+//    }
+//    catch (Type t1){
+//        //handle that exception
+//
+//    }
+//    catch(Type t2){
+//
+//        //hadle that exdeption
+//        // t1,t2 .... should be subclass of exception class
+//    }
+//
+//
+//
+//    finally {
+//        //used for common code
+//
+//        //like suppose some common code we wrote in catch(Type t1 ) similarly sone in catch(Type t2) these we can write common code in
+//        //finally block
+//    }
+//
+//
+//
+//    throws-> this we use when we dont know how to handle th exception ,so whenever any function calls it it has to handle it or pass it to his caller
+//
+     //the function may through one or more exception its responsibility of the calling function to handle the exception
 
 
 
-    try {
-        //code that can throw an exception put it inot this block
+
+//    vod fun() throws FileNotFoundException{  //so this code might throw an error so whatever function calls it ,it has to hadle it explicitly or pass it to its upper function.
+//        Scanner sc=new Scanner("file.text");
+//    }
+//
+//
+//
+//
+//    throw ->used for throwing an exception like if someone enters age lesss than 18 throw exception .
+//
+
+    //throws & thorw should have same excception match .
+
+    public void exampleMethod() throws ArithmeticException {
+        throw new ArithmeticException("Division by zero");
     }
-    catch (Type t1){
-        //handle that exception
 
-    }
-    catch(Type t2){
-
-        //hadle that exdeption
-        // t1,t2 .... should be subclass of exception class
-    }
-
-
-
-    finally {
-        //used for common code
-
-        //like suppose some common code we wrote in catch(Type t1 ) similarly sone in catch(Type t2) these we can write common code in
-        //finally block
-    }
-
-
-
-    thorws-> this we use when we dont know how to handle th exception ,so whenever any function calls it it has to handle it or pass it to his caller
-
-    vod fun() throws FileNotFoundException{  //so this code might throw an error so whatever function calls it ,it has to hadle it explicitly or pass it to its upper function.
-        Scanner sc=new Scanner("file.text");
-    }
-
-
-
-
-    throw ->used for throwing an exception like if someone enters age lesss than 18 throw exception .
-
-userdefined exception-> these are the subclass of exception class.
-
-
-
-
+//userdefined exception-> these are the subclass of exception class.
+//
+//
+//
+//
+//}
 }
