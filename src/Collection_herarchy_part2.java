5c5d1343a3d34f9d97e325249b6d015b3a6ce913
pryas32 <shuklaaayush805@gmail.com>
2025-01-27 22:38:01 +0530

    commit final

---
+++ src/Collection_herarchy_part2.java
@@ -0,0 +1,33 @@
+public class Collection_herarchy_part2 {
+
+
+    Iterable Interface ->Colelction interface extends iterable interface
+//
+    Colection interface contains method like add ,contains wich is implemented by other classes like list,queue,dequeue,set;
+
+    Colections->it is a class which contains basicjn utility functions like max.min.
+
+
+    List ->arraylist(dynamic size array) ,linkedlist(doubly linked list),stack(arraydequeue) and vector(arraylist)->are used in single threaded env.
+
+    set ->hashset(implementation of hashing),treeset (self balancy red black tree (logn) sorted order ),linkedhashset(contains hashmap adn linkedlist)order of insertion
+       queue->linkedlist,arraydequeu ,priortiyqueue
+    dequeue->linkedlist ,arraydequeue
+    map ->hashmap,treemap,linkedhashmap
+
+/// /
+    Colections ->it contains of basic utility functiois like max,min whcih can be applied on other collctions.
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
