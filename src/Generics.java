a8edea5c6276825ab7474fd61670dbb7c496cbb2
pryas32 <shuklaaayush805@gmail.com>
2025-01-27 23:14:30 +0530

    commit final

---
+++ src/Generics.java
@@ -0,0 +1,20 @@
+public class Generics {
+//generics means write code once and can be used for multiple data type .
+   // eg for sum pass integer ,float etc and in function  pass function as generics
+//like sum(<T>int i)
+//they can be used for collections like sort function can be used for any data type like integer ,float
+    //can be used for method ,interface(List,set,map,queue,dequeue anc class(like arraylist etc
+
+
+   // Object class is the root for every noin primitive data type
+
+
+
+
+}
+
+
+
+
+
+
