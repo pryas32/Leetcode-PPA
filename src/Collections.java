aa64454742ee6080769c8ea25e351fb1463dd77b
pryas32 <shuklaaayush805@gmail.com>
2025-01-29 00:00:50 +0530

    commit final

---
+++ src/Collections.java
@@ -0,0 +1,31 @@
+public interface Collections extends Iterable {
+
+    int size ();
+    boolean isEmpty();
+    boolean cotains();
+    Object []toarray();
+    T[] toarray(T []arr);//convert collections to array
+    boolean add(E e);
+    boolean remove(Object o);
+    //bulk operations
+addAll();
+removeAll();
+containsAll();
+removeIf();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
