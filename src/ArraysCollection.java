7ce95d703f7753701adebb391e683ca5509e4f0b
pryas32 <shuklaaayush805@gmail.com>
2025-02-04 00:27:01 +0530

    commit final

---
+++ src/ArraysCollection.java
@@ -0,0 +1,23 @@
+public class ArraysCollection {
+
+    //Arrays->its a utility functions that provide functions for nirmal arrays not for collection.
+
+    //meothods like
+    //.asList()->it provide list over same array
+    //.compare();
+    //mismatch(arr1,arr2)
+    //.fill()
+     //Arrays.sort()->sort primitive type only in increasing order
+    //binary_search()
+    //toString()->print the array in one go
+    //stream()
+    //equals()
+
+
+
+
+
+
+
+
+}
