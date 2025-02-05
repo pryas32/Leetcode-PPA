fec3cb36242228cf144734324d187ee7788017e8
pryas32 <shuklaaayush805@gmail.com>
2025-02-05 22:47:03 +0530

    commit final

---
+++ src/arraysaslist.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+
+public class arraysaslist {
+
+    int x;
+    int y;
+
+    arraysaslist(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+
+
+    @Override
+    public String toString() {
+        return x+" "+y;
+    }
+
+    public static void main(String args[])
+{
+arraysaslist l[]={
+        new arraysaslist(1,23),new arraysaslist(11,232),
+        new arraysaslist(123,2343)
+};
+
+
+System.out.println(Arrays.toString(l));
+//Arrays.toString()->gives string representation of the array
+ //its both for primitive and  non primitiev type
+
+
+
+    int arr[]={1,2,3,4,5};
+
+    System.out.println(Arrays.toString(arr));
+
+Integer arra[]={1,2,3,4};
+
+System.out.println(Arrays.toString(arra));
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}
